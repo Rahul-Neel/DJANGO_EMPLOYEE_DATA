745f28ae54242cb57c5aa67de85391eb397015ac
Rahul-Neel <rahulneelakantam12345@.com>
2025-07-25 15:00:59 +0530

    All Employees Data Project files are added

--- practice/settings.py
+++ practice/settings.py
@@ -141,12 +141,4 @@ MEDIA_ROOT = BASE_DIR /'media'
 
 
 
-if os.getenv('RENDER', None):  # Only run on Render
-    django.setup()
-    from django.contrib.auth.models import User
-    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-    email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpassword123")
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
 
